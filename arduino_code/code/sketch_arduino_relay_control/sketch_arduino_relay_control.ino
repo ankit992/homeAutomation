f062378a910e374bdeb4035009a0369109dcf3cd
Ankit Arora <ankit992@live.in>
2017-10-27 00:43:49 +0530

    if else for multiple relays

--- arduino_code/code/sketch_arduino_relay_control/sketch_arduino_relay_control.ino
+++ arduino_code/code/sketch_arduino_relay_control/sketch_arduino_relay_control.ino
@@ -1,13 +1,25 @@
-int relay1 = 7;                 
-int relay2 = 8;                 
-int relay3 = 9;                 
-int relay4 = 10;   
+int relay1 = 2;                 
+int relay2 = 3;                 
+int relay3 = 4;                 
+int relay4 = 5;
+int relay5 = 6;
+int relay6 = 7;
+int relay7 = 8;
+int relay8 = 9;
+   
 char junk;
 String inputString="";              
 
 void setup() {
- Serial.begin(9600);         
- pinMode(relay2, OUTPUT);
+   Serial.begin(9600); 
+   pinMode(relay1, OUTPUT);
+   pinMode(relay2, OUTPUT);
+   pinMode(relay3, OUTPUT);
+   pinMode(relay4, OUTPUT);
+   pinMode(relay5, OUTPUT);
+   pinMode(relay6, OUTPUT);
+   pinMode(relay7, OUTPUT);
+   pinMode(relay8, OUTPUT);
 }
 
 void loop() {
@@ -20,11 +32,23 @@ void loop() {
     Serial.println(inputString);
     while (Serial.available() > 0)  
     { junk = Serial.read() ; }      // clear the serial buffer
-    if(inputString == "a"){         //in case of 'a' turn the LED on
-      digitalWrite(relay2, HIGH);  
-    }else if(inputString == "b"){   //incase of 'b' turn the LED off
-       digitalWrite(relay2, LOW);
+    if(inputString == "1o"){ digitalWrite(relay1, HIGH);}
+    else if(inputString == "1f"){ digitalWrite(relay1, LOW);}
+    else if(inputString == "2o"){ digitalWrite(relay2, HIGH);}
+    else if(inputString == "2f"){ digitalWrite(relay2, LOW);}
+    else if(inputString == "3o"){ digitalWrite(relay3, HIGH);}
+    else if(inputString == "3f"){ digitalWrite(relay3, LOW);}
+    else if(inputString == "4o"){ digitalWrite(relay4, HIGH);}
+    else if(inputString == "4f"){ digitalWrite(relay4, LOW);}
+    else if(inputString == "5o"){ digitalWrite(relay5, HIGH);}
+    else if(inputString == "5f"){ digitalWrite(relay5, LOW);}
+    else if(inputString == "6o"){ digitalWrite(relay6, HIGH);}
+    else if(inputString == "6f"){ digitalWrite(relay6, LOW);}
+    else if(inputString == "7o"){ digitalWrite(relay7, HIGH);}
+    else if(inputString == "7f"){ digitalWrite(relay7, LOW);}
+    else if(inputString == "8o"){ digitalWrite(relay8, HIGH);}
+    else if(inputString == "8f"){ digitalWrite(relay8, LOW);}
     }
     inputString = "";              \
 }
-}
+
