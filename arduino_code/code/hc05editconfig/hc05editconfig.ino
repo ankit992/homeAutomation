5c7ef5a463ea090476f3d27b1314ca8e81559cba
Ankit Arora <ankit992@live.in>
2017-10-26 23:49:09 +0530

    bluetooth name and password finally updated

--- arduino_code/code/hc05editconfig/hc05editconfig.ino
+++ arduino_code/code/hc05editconfig/hc05editconfig.ino
@@ -1,21 +1,26 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial(0, 1); // RX | TX
+SoftwareSerial BTSerial( 10, 9 ); // RX | TX
 
 void setup() {
   pinMode(2,OUTPUT);
-  // put your setup code here, to run once:
+//  // put your setup code here, to run once:
   digitalWrite(2,HIGH);
   Serial.begin(9600);
-  Serial.println("Enter AT commands : ");
+      Serial.println("Enter AT commands : ");
   BTSerial.begin(38400);
 }
 
 void loop() {
   // Keep reading from HC-05 and send to Arduino Serial Monitor
-  if (BTSerial.available())
+  if (BTSerial.available()){
     Serial.write(BTSerial.read());
+//    Serial.println(BTSerial.read()+"asdasd");
+    }
 
   // Keep reading from Arduino Serial Monitor and send to HC-05
-  if (Serial.available())
-    BTSerial.write(Serial.read());
+  if (Serial.available()){
+//        Serial.println(Serial.read());
+        BTSerial.write(Serial.read());
+    }
 }
+
